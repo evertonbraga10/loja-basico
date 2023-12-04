9ef31c4ca22e154b081bea48bd6b424861e84414
evertonbraga10 <112216426+evertonbraga10@users.noreply.github.com>
2023-12-04 00:44:39 -0300

    btn-wpp

--- js/custom.js
+++ js/custom.js
@@ -58,3 +58,21 @@ Line 47:10 Transforming const to the configured target environment ("es5") is no
   // ^^ Keep your scripts inside this IIFE function call to avoid leaking your
   // variables into the global scope.
 })();
+
+// wpp btn
+
+const content = document.querySelector('[data-shrink="yes"]')
+
+const span = document.querySelector('[data-shrink="yes"] span')
+
+span.classList.add('aparecer')
+
+setInterval(function() {
+ content.classList.toggle('shrink')
+},5000)
+
+setTimeout(function() {
+    setInterval(function() {
+        span.classList.toggle('aparecer')
+       },5000)
+},200)
